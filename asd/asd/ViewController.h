827b88620cacfd1dcacfd5ea2f4535f4cc84dec3
kufufu <lcpgs@126.com>
2015-08-14 10:03:54 +0800

    添加工程

---
+++ asd/asd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asd
+//
+//  Created by kufufu on 15/8/14.
+//  Copyright (c) 2015年 kufufu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ asd/asd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  asd
+//
+//  Created by kufufu on 15/8/14.
+//  Copyright (c) 2015年 kufufu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
