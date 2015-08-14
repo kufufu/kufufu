fcc26c802960b786c1cc83f03fe26ff8ce8aed92
kufufu <lcpgs@126.com>
2015-08-14 10:48:24 +0800

    添加123

--- asd/asd/ViewController.m
+++ asd/asd/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello Kufufu");
+    NSLog(@"123");
 }
 
 - (void)didReceiveMemoryWarning {
